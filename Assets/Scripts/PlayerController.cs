1a529512b396520e8131fd765d26d66d23d29bbc
RoseDuf <rose.dufresne@outlook.com>
2020-02-04 21:38:59 -0500

    added movement and animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,8 +21,8 @@ public class PlayerController : MonoBehaviour
     public int allowed_to_move;
 
     // References to other components (can be from other game objects!)
-    Animator mAnimator;
-    Rigidbody2D rigidBody2D;
+    Animator animator;
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Awake()
@@ -35,8 +35,8 @@ public class PlayerController : MonoBehaviour
         allowed_to_move = 0;
 
         // Get references to other components and game objects
-        mAnimator = GetComponent<Animator>();
-        rigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -51,39 +51,23 @@ public class PlayerController : MonoBehaviour
         //is_grabbing = grab_pressed;
         //grab_pressed = false;
 
-        FaceDirection();
-        if (allowed_to_move <= 0)
-            MoveCharacter();
+        MoveCharacter();
 
 
     }
 
     private void HandleInput()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        movement_vector.x = Input.GetAxisRaw("Horizontal");
+        movement_vector.y = Input.GetAxisRaw("Vertical");
 
-        is_moving = horizontal != 0 || vertical != 0;
+        is_moving = movement_vector.x != 0 || movement_vector.y != 0;
 
-        if (Input.GetButtonDown("Run"))
-        {
-            isWalking = false;
-        }
-        else
-        {
-            isWalking = true;
-        }
+        if (Input.GetButtonDown("Run")) { isWalking = false;}
+        else { isWalking = true; }
 
         //if (Input.GetButtonDown("Grab")) grab_pressed = true;
         //drop_pressed = Input.GetButtonDown("Drop");
-
-        input_vector = new Vector2(horizontal, vertical);
-    }
-
-    private void FaceDirection()
-    {
-        if (is_moving)
-            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(Vector3.back, input_vector), turnSpeed * Time.fixedDeltaTime);
     }
 
     //Controller for player movement
@@ -94,38 +78,15 @@ public class PlayerController : MonoBehaviour
 
         moveSpeed = isWalking ? mWalkSpeed : mRunSpeed;
 
-        if (friction >= 1)
-            movement_vector = Vector2.Lerp(movement_vector, input_vector, 0.125f);
-        else
-            movement_vector = Vector2.Lerp(movement_vector, input_vector, friction * Time.deltaTime);
-
-        rigidBody2D.velocity = (movement_vector * moveSpeed * Time.deltaTime);
+        rb.MovePosition(rb.position + movement_vector * moveSpeed * Time.deltaTime);
     }
 
     //Animation controller
     void Animate()
     {
-        //if (!mGrounded)
-        //{
-        //    mSwimming = true;
-        //}
-        //else
-        //{
-        //    mSwimming = false;
-        //}
-        //if (mRigidBody2D.velocity.y < -2.0f)
-        //{
-        //    mFalling = true;
-        //}
-        //else
-        //{
-        //    mFalling = false;
-        //}
-
-        //mAnimator.SetBool("mSwimming", mSwimming);
-        //mAnimator.SetBool("mMoving", mMoving);
-        //mAnimator.SetBool("mFalling", mFalling);
-        //mAnimator.SetBool("mHurt", mHurt);
+        animator.SetFloat("horizontal", movement_vector.x);
+        animator.SetFloat("vertical", movement_vector.y);
+        animator.SetFloat("speed", movement_vector.sqrMagnitude);
     }
 
 }
