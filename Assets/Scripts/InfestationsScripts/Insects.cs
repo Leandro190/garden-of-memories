b81e6728e9d5e382b10b1f92efed6e78d8f260c6
Jean Lamarre <jonny.lamarre@hotmail.com>
2020-03-01 14:33:15 -0500

    Infestation: added scene and road mesh

--- Assets/Scripts/InfestationsScripts/Insects.cs
+++ Assets/Scripts/InfestationsScripts/Insects.cs
@@ -7,7 +7,7 @@ public class Insects : MonoBehaviour
 {
     private List<GameObject> insects = new List<GameObject>();
 
-    public int InsectNumber;
+    public int InsectNumber = 200;
     [SerializeField] private GameObject InsectPrefab;
     [SerializeField] private PathCreator pathCreator;
 
@@ -15,7 +15,7 @@ public class Insects : MonoBehaviour
 
     private void Start()
     {
-        InsectNumber = (int)pathCreator.path.length * 10;
+        InsectNumber = (int)(pathCreator.path.length * 15f);
     }
 
     void Update()
@@ -24,9 +24,11 @@ public class Insects : MonoBehaviour
         {
             spawnTime += 0.01f;
             insects.Add(Instantiate(InsectPrefab, this.transform));
+            insects[insects.Count - 1].GetComponent<PathFollower2D>().pathCreator = pathCreator;
             insects[insects.Count - 1].transform.GetChild(0).transform.position = new Vector3(insects[insects.Count - 1].transform.GetChild(0).transform.position.x,
-                                                                                                Random.Range(-0.27f, 0.27f),
+                                                                                                Random.Range(0.05f, 0.50f),
                                                                                                 insects[insects.Count - 1].transform.GetChild(0).transform.position.z);
+            
             InsectNumber--;
         }
     }
