b440f3580c74c86dfd2ad94792880fb332b611b0
AdeThorn <adeend1234@gmail.com>
2020-04-11 10:41:36 -0400

    Finished Menu Scene

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+
+   public void PlayGame() //function to load next level
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  //loads next scene in queue
+    }
+        
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit(); //closes down program
+    }
+}
