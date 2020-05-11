5917abfd1864c62c498559957f11a9f166e557b0
RoseDuf <rose.dufresne@outlook.com>
2020-05-11 14:59:07 -0400

    Gonna have to redo puzzle

---
+++ Assets/BackGround.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    SpriteRenderer overlay;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        overlay = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<PlayerController>().won)
+        {
+            overlay.color = new Color (46f,46f,46f,0f);
+        }
+    }
+}
---
+++ Assets/Roots.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roots : MonoBehaviour
+{
+    [SerializeField] Sprite dead_tree_root;
+    [SerializeField] Sprite alive_tree_root;
+
+    SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.sprite = dead_tree_root;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.parent.GetComponent<Tree>().isAlive)
+        {
+            sprite.sprite = alive_tree_root;
+        }
+        else
+        {
+            sprite.sprite = dead_tree_root;
+        }
+    }
+}
